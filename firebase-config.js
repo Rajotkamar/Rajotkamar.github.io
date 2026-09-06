86ecf00c5d99da0368aa1293c6243a2cccb8c779
AR INOVATION HUB <rajotkamar@gmail.com>
2026-09-06 16:30:45 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,11 +9,11 @@
   privileged secrets in this file.
 */
 window.AR_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAcucTIMEUgiFc9s33iuGLdXGrwTpqdEv8",
-  authDomain: "smart-energy-meter-628bc.firebaseapp.com",
-  databaseURL: "https://smart-energy-meter-628bc-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "smart-energy-meter-628bc",
-  storageBucket: "smart-energy-meter-628bc.firebasestorage.app",
-  messagingSenderId: "1064556756523",
-  appId: "1:1064556756523:web:d15b8232949d7fdba2346e"
+  apiKey: "AIzaSyB_BLDDHYqn8JlEAtINalVpXUETxtwrII8",
+  authDomain: "enrge-meter.firebaseapp.com",
+  databaseURL: "https://enrge-meter-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "enrge-meter",
+  storageBucket: "enrge-meter.firebasestorage.app",
+  messagingSenderId: "587918706391",
+  appId: "1:587918706391:web:966783ec61938c0ee0d292"
 };
